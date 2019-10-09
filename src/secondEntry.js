1bd50e1b54156493932d1154f423f691fc64da2f
andy770921 <andy770921@yahoo.com.tw>
2019-10-09 21:04:03 +0800

    second entry page change to HashRouter

--- src/secondEntry.js
+++ src/secondEntry.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import SecondNavbarUi from "./secondNavbarUI";
 import PlayUi from "./components/playUI";
 import RankUi from "./components/rankUI";
-import { BrowserRouter , Route } from 'react-router-dom';
+import { HashRouter , Route } from 'react-router-dom';
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import gameReducer from "./reducers/gameReducer";
@@ -14,14 +14,14 @@ const store = createStore(gameReducer);
 class SecondApp extends Component {
     render() {
         return (
-            <BrowserRouter>
+            <HashRouter>
             <div className="app-content container">
                 <h1 className="blue-text center"> Puzzle Game </h1>
                 <SecondNavbarUi />
                 <Route exact path="/" component={PlayUi} />
                 <Route path="/ranking" component={RankUi} />
             </div>
-            </BrowserRouter>
+            </HashRouter>
         )
     }
 }
